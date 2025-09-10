ff1b19e248b1d5f0a85d8219470ab5c354433a0a
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-09-10 16:47:12 +0700

    Три кавычки

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return '<!DOCTYPE html>' \
-        '<html lang="en">' \
-        '<body>' \
-            '<h1>web-сервер на flask</h1>' \
-        '</body>' \
-        '</html>'
+    return '''<!DOCTYPE html>
+        <html lang="en">
+        <body>
+            <h1>web-сервер на flask</h1>
+        </body>
+        </html>'''
