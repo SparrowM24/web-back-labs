352e2cbd71b7d09d5472b8f6676728b282116a23
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-09-24 17:02:28 +0700

    Имя студента

--- app.py
+++ app.py
@@ -350,4 +350,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Дьячкова Алиса'
+    return render_template('example.html', name=name)
