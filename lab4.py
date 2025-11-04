0360752a2aeb940b6b3a8a7ce0715a074bac9596
Алиса Дьячкова <sparrowm24@mail.ru>
2025-11-04 16:29:26 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -24,3 +24,50 @@ def div():
     else:
         result = x1 / x2
         return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/add', methods = ['POST'])
+def add():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    x1 = int(x1) if x1 != '' else 0
+    x2 = int(x2) if x2 != '' else 0
+    result = x1 + x2
+    return render_template('lab4/add.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/mul', methods = ['POST'])
+def mul():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    x1 = int(x1) if x1 != '' else 1
+    x2 = int(x2) if x2 != '' else 1
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub', methods = ['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быль заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/expo', methods = ['POST'])
+def expo():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/expo.html', error='Оба поля должны быль заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/expo.html', error='Оба должны быль отличны от нуля!')
+    else:
+        result = x1 ** x2
+        return render_template('lab4/expo.html', x1=x1, x2=x2, result=result)
