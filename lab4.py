6fad48abbd5f03ba8663cc2976c6edf1e41f5afc
Алиса Дьячкова <sparrowm24@mail.ru>
2025-11-05 11:29:43 +0700

    Заказ зерна

--- lab4.py
+++ lab4.py
@@ -180,3 +180,60 @@ def fridge():
                          message=message, 
                          snowflakes=snowflakes,
                          temperature=temperature)
+
+
+@lab4.route('/lab4/grain_order', methods=['GET', 'POST'])
+def grain_order():
+    if request.method == 'GET':
+        return render_template('lab4/grain_order.html')
+    grain_type = request.form.get('grain_type')
+    weight = request.form.get('weight')
+    if not grain_type:
+        return render_template('lab4/grain_order.html', 
+                             error="Выберите тип зерна")
+    if not weight:
+        return render_template('lab4/grain_order.html', 
+                             error="Введите вес заказа",
+                             grain_type=grain_type)
+    try:
+        weight_float = float(weight)
+    except ValueError:
+        return render_template('lab4/grain_order.html', 
+                             error="Вес должен быть числом",
+                             grain_type=grain_type)
+    if weight_float <= 0:
+        return render_template('lab4/grain_order.html', 
+                             error="Вес должен быть положительным числом",
+                             grain_type=grain_type)
+    if weight_float > 100:
+        return render_template('lab4/grain_order.html', 
+                             error="Извините, такого объёма сейчас нет в наличии",
+                             grain_type=grain_type)
+    prices = {
+        'barley': 12000,  # ячмень
+        'oats': 8500,     # овёс
+        'wheat': 9000,    # пшеница
+        'rye': 15000      # рожь
+    }
+    grain_names = {
+        'barley': 'ячмень',
+        'oats': 'овёс', 
+        'wheat': 'пшеница',
+        'rye': 'рожь'
+    }
+    price_per_ton = prices[grain_type]
+    total_cost = weight_float * price_per_ton
+    discount = 0
+    if weight_float > 10:
+        discount = total_cost * 0.10
+        total_cost -= discount
+    grain_name = grain_names[grain_type]
+    return render_template('lab4/grain_order.html', 
+                         success=True,
+                         grain_name=grain_name,
+                         weight=weight_float,
+                         total_cost=total_cost,
+                         discount=discount,
+                         grain_type=grain_type)
+
+
