b43b3b73a5bf0fe4ba3a9a75466893a7d1cf71a6
Алиса Дьячкова <sparrowm24@mail.ru>
2025-12-08 18:50:59 +0700

    Самостоятельные задания

--- db/models.py
+++ db/models.py
@@ -1,12 +1,11 @@
 from . import db
 from flask_login import UserMixin
 
-class users(db.Model, UserMixin):  # ← ВАЖНО: UserMixin добавляет is_active
+class users(db.Model, UserMixin):  
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(162), nullable=False)
     
-    # Flask-Login требует эти свойства
     @property
     def is_active(self):
         """Все пользователи активны по умолчанию"""
