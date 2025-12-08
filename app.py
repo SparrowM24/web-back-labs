50ea8fad8a748910886de8134f4ec7fe84b701f9
Алиса Дьячкова <sparrowm24@mail.ru>
2025-12-08 10:22:41 +0700

    Правки2

--- app.py
+++ app.py
@@ -37,13 +37,12 @@ if app.config['DB_TYPE'] == 'postgres':
     db_user = 'alice_dyachkova_orm'
     db_password = '123'
     host_ip = '127.0.0.1'
-    host_port = 5433
+    host_port = 5432
     
     app.config['SQLALCHEMY_DATABASE_URI'] = \
         f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
 
 else:
-    import os.path as path
     dir_path = path.dirname(path.realpath(__file__))
     db_path = path.join(dir_path, "alice_dyachkova_orm.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
